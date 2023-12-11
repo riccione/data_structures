6967c842b86a0720e3657ce375dad1271aabc2a2
Zero <zero@zero-ms7c56.ignes-zero.com>
2023-12-10 22:07:41 -0500

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Data structures with C
+
+## Compile
+`gcc -Wall -lm <file.c> -o <file>`
+
+## linked list
+### singly linked list
---
+++ linked_list/singly_linked_list.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int size = 0;
+
+typedef struct node {
+    int val;
+    struct node* next;
+} node;
+
+// traverse the list and print elements of linked list
+void print_list(node* head) {
+    node* current = head;
+    while(current != NULL) {
+        printf("%d -->", current->val);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+// traverse the linked list and count elements
+int len(node* head) {
+    node* current = head;
+    int x = 0;
+    while(current != NULL) {
+        x++;
+        current = current->next;
+    }
+    return x;
+}
+
+// append an element at the end of the linked list
+void append(node* head, int val) {
+    node* current = head;
+    while(current->next != NULL) {
+        current = current->next;
+    }
+    current->next = (node*) malloc(sizeof(node));
+    current->next->val = val;
+    current->next->next = NULL;
+    size++;
+}
+
+/*
+ * val == -1 <= remove last element
+ */
+int pop(node* head, int val) {
+    int rval = 0;
+    node* current = head;
+    
+    // no elements
+    if (size < 1) {
+        return -1;
+    }
+
+    // if there is only one element
+    if (current->next == NULL) {
+        rval = current->val;
+        free(current);
+        size--;
+        return rval;
+    }
+    
+    if (val == -1) {
+        // get node before tail
+        while (current->next->next != NULL) {
+            current = current->next;
+        }
+
+        rval = current->next->val;
+        free(current->next);
+        current->next = NULL;
+        size--;
+    }
+    return rval;
+}
+
+int contain(node* head, int val) {
+    int counter = 0;
+    while(head != NULL) {
+        if (head->val == val) {
+            return 1; 
+        }
+        head = head->next;
+        counter++;
+    }
+    return -1;
+}
+
+int main() {
+    node* head = NULL;
+    head = (node*) malloc(sizeof(node));
+    if (head == NULL) {
+        return 1;
+    }
+    
+    head->val = 1;
+    size++;
+    head->next = (node*) malloc(sizeof(node));
+    head->next->val = 2;
+    head->next->next = NULL;
+    size++;
+    
+    // add an element at the end of the linked list
+    append(head, 100);
+
+    print_list(head);
+    printf("size of the list: %d\n", len(head));
+    printf("size of the list from global var size: %d\n", size);
+
+    printf("Linked list contains %d: %d\n", 2, contain(head, 2));
+
+    // pop last element
+    pop(head, -1);
+    print_list(head); 
+
+    return EXIT_SUCCESS;
+}
